e21cb4bff496b4c9e69a8c3f4df6d3fdcd6fb104
Wijayaac <wijayaac25@gmail.com>
2024-04-08 14:31:55 +0700

    style: form & confirmation color

--- components/header.jsx
+++ components/header.jsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <header className='sticky top-0 z-50 w-full px-20 py-4 bg-gray-dark text-white'>
+    <header className='sticky top-0 z-50 w-full md:px-20 px-8 py-4 bg-nw-gray text-white'>
       <div className='max-w-[840px] mx-auto flex flex-wrap justify-between '>
         <h1>Frontend Mentors</h1>
         <ul className='flex gap-4'>
